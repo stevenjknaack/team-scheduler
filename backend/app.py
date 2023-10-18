d9f3a90f43849877f743ad1fc1991ad5212465c1
Dante Katz Andrade <dantebr6@gmail.com>
2023-10-18 12:24:55 -0500

    This should be updated correctly now.

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 from flask_cors import CORS
 import mysql.connector
 import mysecrets
-#import bcrypt
+import bcrypt
 import os
 
 app = Flask(__name__, root_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
@@ -69,6 +69,11 @@ def profile():
         return redirect(url_for('index'))
     return render_template('profile.html', username=session['username'])
 
+@app.route('/signup')
+def signup() :
+    if 'username' in session :
+        return redirect(url_for('profile'))
+    return render_template('signup.html')
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
@@ -98,28 +103,40 @@ def signup_request():
     db.close()
     return jsonify(status='success')
     #return render_template('login.html')
+@app.route('/saveEvent', methods=['POST', 'GET'])
+def saveEvent():
+    if request.method == 'POST':
+        # Get event data from the HTML form
+        event_name = request.form.get('event_name')
+        event_day = request.form.get('event_day')
+        event_month = request.form.get('event_month')
+        event_year = request.form.get('event_year')
 
-    # Connect to the database
-    db = get_db_connection()
-    cursor = db.cursor()
+        # Combine the date components into a single string
+        time_range = event_day + ' ' + event_month + ' ' + event_year
 
-    # Insert the event data into the "savedEvent" table
-    query = "INSERT INTO savedEvent (event_name, time_range) VALUES (%s, %s);"
-    values = (event_name, time_range)
-    cursor.execute(query, values)
+        # Connect to the database
+        db = get_db_connection()
+        cursor = db.cursor()
 
-    # Commit the changes to the database
-    db.commit()
+        # Insert the event data into the "savedEvent" table
+        query = "INSERT INTO savedEvent (event_name, time_range) VALUES (%s, %s);"
+        values = (event_name, time_range)
+        cursor.execute(query, values)
 
-    # Close the cursor and the database connection
-    cursor.close()
-    db.close()
+        # Commit the changes to the database
+        db.commit()
 
-    # Return a success JSON response
-    return jsonify(status='success')
- 
+        # Close the cursor and the database connection
+        cursor.close()
+        db.close()
+
+        # Return a success JSON response
+        return jsonify(status='success')
+
     # Handle GET requests (if needed)
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=6969)  # Running the app on localhost:6969

64410e601c007c956c3f145283ea7a21a5018902
wrldbf <98567119+wrldbf@users.noreply.github.com>
2023-10-18 13:15:17 -0500

    add signup and modify signin

--- backend/test.py
+++ backend/test.py
@@ -11,7 +11,11 @@ class MyTestCase(unittest.TestCase):
         result = self.app.get('/')
         # Make your assertions
         self.assertEqual(1,1)
+
+    def test_signup_page(self):
+        result = self.app.get('/signup')
+        self.assertEqual(result.status_code, 200)
       
     def test_login(self):
         result = self.app.get('/login')
-        self.assertFalse(False)
+        self.assertEqual(result.status_code, 200)

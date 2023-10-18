c668488882d5bcd98901484b4ec85c3ab008df09
SJKNAACK <sjknaack@wisc.edu>
2023-10-17 21:36:56 -0500

    Updated database file used to build current db

--- data/db.sql
+++ data/db.sql
@@ -1,35 +1,35 @@
-CREATE TABLE `savedEvent` (
-  `event_id` integer,
-  `event_name` varchar(255),
-  `time_range` varchar(255),
-  `event_description` text,
-  `admin_id` integer,
-  `invitee_id` integer
+CREATE DATABASE `10stars`;
+USE `10stars`;
+
+CREATE TABLE `user` (
+  `user_id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `email` VARCHAR(255) UNIQUE,
+  `username` VARCHAR(255),
+  `password` VARCHAR(255)
 );
 
-CREATE TABLE `userView` (
-  `user_id` integer PRIMARY KEY,
-  `saved_event_id` integer
+CREATE TABLE `saved_event` (
+  `event_id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `event_name` VARCHAR(255),
+  `start_date` DATE,
+  `end_date` DATE,
+  `start_time` TIME,
+  `end_time` TIME,
+  `event_description` TEXT,
+  `owner_id` INTEGER,
+  FOREIGN KEY (`owner_id`) REFERENCES `user` (`user_id`)
+);
+
+CREATE TABLE `user_view` (
+  `user_id` INTEGER PRIMARY KEY,
+  `saved_event_id` INTEGER,
+  `user_role` VARCHAR(10),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  FOREIGN KEY (`saved_event_id`) REFERENCES `saved_event` (`event_id`)
 );
 
 CREATE TABLE `invitee` (
-  `invitee_id` integer,
-  `queue_number` integer
+  `invitee_id` INTEGER,
+  `queue_number` INTEGER,
+  FOREIGN KEY (`invitee_id`) REFERENCES `user` (`user_id`)
 );
-
-CREATE TABLE `users` (
-  `id` integer PRIMARY KEY,
-  `email` varchar(255),
-  `username` varchar(255),
-  `password` varchar(255)
-);
-
-ALTER TABLE `userView` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `savedEvent` ADD FOREIGN KEY (`event_id`) REFERENCES `userView` (`saved_event_id`);
-
-ALTER TABLE `invitee` ADD FOREIGN KEY (`invitee_id`) REFERENCES `savedEvent` (`invitee_id`);
-
-ALTER TABLE `invitee` ADD FOREIGN KEY (`invitee_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `savedEvent` ADD FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`);

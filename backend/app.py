35ef45062656a48f242469fec6f3d9dd45b5fa05
SJKNAACK <sjknaack@wisc.edu>
2023-12-17 14:06:19 -0600

    Added config.py and the start of configurating from the options

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,9 @@
 :author : Anwita
 """
 from flask import Flask
+
+from .config import Config
+
 from .blueprints.auth import auth_blueprint
 from .blueprints.events import events_blueprint
 from .blueprints.groups import groups_blueprint
@@ -19,26 +22,19 @@ from .extensions import db, mail
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # load environment
-
-def create_app() -> Flask:
+def create_app(config_class=Config) -> Flask:
     """
     Creates a 10stars Flask app 
 
     :returns: a 10stars Flask app
     """
+    # instantiate and configure app
     path: str = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
     app: Flask = Flask(__name__, root_path=path)
-    app.secret_key = os.getenv('SECRET_KEY')
-    
-    # configure the MYSQL database, relative to the app instance
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object(config_class)
 
-    # initialize the app with the extension
+    # initialize the extensions with app
     db.init_app(app)
-
-    # initialize mail with the app
     mail.init_app(app)
     
     # register blueprints
@@ -48,22 +44,11 @@ def create_app() -> Flask:
     app.register_blueprint(profile_blueprint)
     app.register_blueprint(teams_blueprint)
 
-    # configure Flask-Mail API
-    app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-    app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-    app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
-
-    # mail = Mail(app)
-
     return app
 
 # create and run app
 if __name__ == '__main__':
     app: Flask = create_app()
-    app.run(debug=True, port=int(os.getenv('FLASK_PORT')))
+    app.run(port=int(os.getenv('FLASK_PORT')))
 
 
---
+++ backend/config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # load environment
+
+class Config:
+  # secret key
+  SECRET_KEY = os.getenv('SECRET_KEY')
+
+  # configure the flask_sqlalchemy
+  SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+  # configure Flask-Mail API
+  MAIL_SERVER = os.getenv('MAIL_SERVER')
+  MAIL_PORT = os.getenv('MAIL_PORT')
+  MAIL_USE_TLS = True
+  MAIL_USE_SSL = False
+  MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+  MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+  MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+
+  # move to development config later
+  FLASK_DEBUG = True
